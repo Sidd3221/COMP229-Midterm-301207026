30f45997230ee2e7f5eef0ff4ddd7979909aba83
Siddharth Verma <vermasiddharth51@gmail.com>
2022-10-24 15:48:13 -0400

    Initialized book database on Mongo Atlas and established connection to database.

--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,9 @@
-export const MongoURI = "mongodb://localhost:27017/media";
+// Filename: config.js
+// Student Name: Siddharth Verma
+// Student ID: 301207026
+// Date: October 1, 2022
+
 export const Secret = "someSecret";
+
+//Exporting the connection to Mongo Atlas database to be used whilst setting up the app in server.js
+export const MongoURI  = "mongodb+srv://siddharth:sidd1998@comp229-f2022-week5.zd4uzcb.mongodb.net/BookDatabase?retryWrites=true&w=majority";
