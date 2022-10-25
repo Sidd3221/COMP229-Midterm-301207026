a7bfccf099fb427207ba83d7f6acfaefe4999d2b
Siddharth Verma <vermasiddharth51@gmail.com>
2022-10-25 11:46:57 -0400

    Added code to process edit and delete requests

--- server/controllers/books.js
+++ server/controllers/books.js
@@ -42,23 +42,53 @@ export function processAddPage(req, res, next) {
 
 // GET the Book Details page in order to edit an existing Book
 export function displayEditPage(req, res, next) {
-
-    /*****************
-     * ADD CODE HERE *
-     *****************/
-
+    let id = req.params.id;
+    
+    booksModel.findById(id,(err, book) =>{
+        if(err){
+            console.error(err);
+            res.end(err);
+        }
+        
+        else{
+            res.render('index', { title: 'Edit book', page: 'books/edit', book: book});
+        }
+    })
 }
 
 // POST - process the information passed from the details form and update the document
 export function processEditPage(req, res, next) {
-    /*****************
-    * ADD CODE HERE *
-    *****************/
+    let id = req.params.id;
+    
+    let updatedBook = booksModel({
+        _id: req.body.id,
+        ...req.body
+    })
+
+    booksModel.updateOne({_id: id }, updatedBook, (err) =>{
+        if(err){
+            console.error(err);
+            res.end(err);
+        }
+        
+        else{
+            res.redirect('/books/list');
+        }
+    })
 }
 
 // GET - process the delete by user id
 export function processDelete(req, res, next) {
-    /*****************
-  * ADD CODE HERE *
-  *****************/
+    let id = req.params.id;
+
+    booksModel.remove({_id: id }, (err) =>{
+        if(err){
+            console.error(err);
+            res.end(err);
+        }
+        
+        else{
+            res.redirect('/books/list');
+        }
+    })
 }
