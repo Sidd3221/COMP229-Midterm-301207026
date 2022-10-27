edc7be467c14600cc5cea0ec914d1999d04ac84d
Siddharth Verma <vermasiddharth51@gmail.com>
2022-10-27 13:09:57 -0400

    Minor edits

--- server/config/config.js
+++ server/config/config.js
@@ -6,5 +6,5 @@
 
 export const Secret = "someSecret";
 
-//Exporting the connection to Mongo Atlas database to be used whilst setting up the app in server.js
+//Exporting the connection to Mongo Atlas database to be used
 export const MongoURI  = "mongodb+srv://siddharth:sidd1998@comp229-f2022-week5.zd4uzcb.mongodb.net/BookDatabase?retryWrites=true&w=majority";
